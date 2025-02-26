41c0f0e17bd5d84d77ac36a727ded009a0f4ed87
柏翔 王 <elijah.wang@nec.com.tw>
2025-02-26 15:11:03 +0800

    chore: 更新組件資訊版本號

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+007f599abf905cd063366c4670dc992541c3c308")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53fe26c9b78a92c942cf6bc1da359cb46211a1b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
