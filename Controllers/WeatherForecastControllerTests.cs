171488f1dfadff1901b1b77bc1195f660e0ae4e2
柏翔 王 <elijah.wang@nec.com.tw>
2025-02-26 15:15:35 +0800

    test: 新增天氣預報控制器的單元測試

---
+++ Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,10 @@
+using Xunit;
+
+public class WeatherForecastControllerTests
+{
+    [Fact]
+    public void HelloWorldTest()
+    {
+        Assert.Equal(1 + 1, 2);
+    }
+}
--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53fe26c9b78a92c942cf6bc1da359cb46211a1b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41c0f0e17bd5d84d77ac36a727ded009a0f4ed87")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
